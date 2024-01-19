2a3b639bf22c5da58a703ba5285428fef95dbcf8
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-01-19 10:41:09 +0000

    Skeleteon of getDirectSuppliersForUser created

--- src/main/kotlin/com/example/Domain.kt
+++ src/main/kotlin/com/example/Domain.kt
@@ -1,4 +1,17 @@
 package com.example
 
 class Domain {
+    fun getDirectSuppliersForUser(userID: String): List<String> {
+        val userRepo = UserRepo()
+        //todo: Handle Errors
+        val companyID: String = userRepo.fetchCompanyIdThatUserBelongsTo(userId)
+
+        val supplyChainRepo = SupplyChainRepo()
+        //todo: Handle Errors
+        val supplyChain: SupplyChain = supplyChainRepo.fetchCompanySupplyChain(companyId)
+
+        val directSupplierIds: List<String> = findDirectSuppliersForCompany(supplyChain, companyID)
+
+        return directSupplierIds
+    }
 }
