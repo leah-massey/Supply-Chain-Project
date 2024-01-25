03bf3f48c3aaed79e4177748b1ce9489788ba7eb
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-01-25 11:06:46 +0000

    Test(SupplyChainRepoJson): when 'O' has no suppliers, an empty supply chain is returned

--- src/test/kotlin/com/example/SupplyChainRepoJSONTest.kt
+++ src/test/kotlin/com/example/SupplyChainRepoJSONTest.kt
@@ -5,19 +5,26 @@ import org.junit.jupiter.api.Test
 
 class SupplyChainRepoJSONTest {
     @Test
-    fun `the supply chain belonging to 'O' is returned`() {
+    fun `when 'O' has a supply chain with suppliers, the supply chain belonging to 'O' is returned`() {
         val underTest = SupplyChainRepoJSON()
         val expected = SupplyChain(directSuppliers = listOf("ZS111"), indirectSuppliers = listOf("ZS222", "ZS333"))
         val actual = underTest.fetchCompanySupplyChain("ZC789")
 
         assertEquals(expected, actual)
     }
-
     @Test
-    fun `an empty supply chain is returned when 'O' is not in the supplyChainRepo`() {
+    fun `when 'O' is not in the supplyChainRepo, an empty supply chain is returned`() {
         val underTest = SupplyChainRepoJSON()
         val expected = SupplyChain(directSuppliers = emptyList(), indirectSuppliers = emptyList())
         val actual = underTest.fetchCompanySupplyChain("ZC781")
+
+        assertEquals(expected, actual)
+    }
+    @Test
+    fun `when 'O' has no suppliers, an empty supply chain is returned`() {
+        val underTest = SupplyChainRepoJSON()
+        val expected = SupplyChain(directSuppliers = emptyList(), indirectSuppliers = emptyList())
+        val actual = underTest.fetchCompanySupplyChain("ZC711")
 
         assertEquals(expected, actual)
     }
